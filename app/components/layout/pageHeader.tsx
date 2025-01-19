22027431ffcc90e57a96b268da398ff7f42bd506
Tanner B. Hess Webber <tb@hesswebber.com>
2025-01-19 13:28:19 -0500

    fix(page header): allows passing an element down for the action

--- app/components/layout/pageHeader.tsx
+++ app/components/layout/pageHeader.tsx
@@ -1,6 +1,7 @@
 import { Flex } from "@chakra-ui/react";
 import { Button } from "@components/ui/button";
 import { Heading } from "@components/ui/heading";
+import { isValidElement, ReactElement, useMemo } from "react";
 // import { useNavigate } from "@tanstack/react-router";
 // import { useCallback } from "react";
 import { IconType } from "react-icons";
@@ -12,24 +13,48 @@ import {
   BreadcrumbRoot,
 } from "../ui/breadcrumb";
 
-interface PageHeaderProps {
-  action?: {
-    handleClick: () => void;
-    icon: IconType;
-    label: string;
-  };
+export interface PageHeaderProps {
+  action?:
+    | ReactElement
+    | {
+        handleClick: () => void;
+        icon: IconType;
+        label: string;
+      };
   crumbs?: BreadcrumbLinkProps[];
   title: string;
 }
 
-export function PageHeader({ action, crumbs, title }: PageHeaderProps) {
-  // const navigate = useNavigate();
-  const ActionIcon = action?.icon;
-  // const handleClick = useCallback(() => {
-  //   navigate({ search: { modal: action?.modalName } }).catch((e: unknown) => {
-  //     console.log(e);
-  //   });
-  // }, [navigate, action?.modalName]);
+export function PageHeader({
+  action: actionProp,
+  crumbs,
+  title,
+}: PageHeaderProps) {
+  const action = useMemo(() => {
+    if (isActionProp(actionProp)) {
+      if (isValidElement(actionProp)) {
+        return actionProp;
+      } else {
+        const ActionIcon = actionProp.icon;
+
+        return (
+          <Button
+            onClick={() => {
+              console.log("hi");
+
+              actionProp.handleClick();
+            }}
+            type="button"
+            variant={"subtle"}
+          >
+            <ActionIcon />
+            {actionProp.label}
+          </Button>
+        );
+      }
+    }
+    return null;
+  }, [actionProp]);
 
   return (
     <Flex as="header" direction={"column"} py="6">
@@ -48,22 +73,24 @@ export function PageHeader({ action, crumbs, title }: PageHeaderProps) {
         <Heading fontFamily="serif" level="1" size="4xl">
           {title}
         </Heading>
-
-        {!!action && (
-          <Button
-            onClick={() => {
-              console.log("hi");
-
-              action.handleClick();
-            }}
-            type="button"
-            variant={"subtle"}
-          >
-            {!!ActionIcon && <ActionIcon />}
-            {action.label}
-          </Button>
-        )}
+        {action}
       </Flex>
     </Flex>
+  );
+}
+
+function isActionProp(
+  value: unknown,
+): value is Required<PageHeaderProps>["action"] {
+  return (
+    typeof value === "function" ||
+    ["label", "icon", "handleClick"].every(
+      (key) =>
+        key in
+        (value as Exclude<
+          PageHeaderProps["action"],
+          ((...args: unknown[]) => unknown) | undefined
+        >),
+    )
   );
 }
